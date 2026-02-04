a83e5d90de688cb3c896877ad0087f95c2c1fcef
NH <noahhulett@my.whccd.edu>
2026-02-04 01:48:14 -0800

    Add project files.

---
+++ COMP003A.LectureActivity4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP003A.LectureActivity4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
